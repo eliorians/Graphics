f135d2c4f4d9d785e882cc37ae1ae30341d9f43d
Eli Orians <eli.orians@gmail.com>
2024-02-08 20:55:26 -0500

    more stuff working yay

--- ray1.2/src/ray/surface/Ellipsoid.java
+++ ray1.2/src/ray/surface/Ellipsoid.java
@@ -34,7 +34,8 @@ public class Ellipsoid extends Surface {
 	protected final Vector3 cAxis = new Vector3(0, 0, 1);
 	public void setCAxis(Vector3 cAxis) { this.cAxis.set(cAxis); this.cAxis.normalize(); }
     
-	//TODO: this
+	//TODO: make this work for rotated ellipsoids
+	//? or does that occur in computeRayDirection
     @Override
     public HitRecord hit(Ray ray, double t0, double t1) 
 	{
@@ -50,16 +51,25 @@ public class Ellipsoid extends Surface {
 		//x,y,z = coords on ellipse (ray)
 		//a,b,c = x,y,z axis length respectfully
 		
-		//todo do calculations for when ellipse is not a perfect sphere?
-		double a = 1;
-		double b = 1;
-		double c = 1;
+		//where min and max are the bounding coords of a box around the ellipsoid
+		double xMin = center.x - a;
+		double xMax = center.x + a;
+		double yMin = center.y - b;
+		double yMax = center.y + b;
+		double zMin = center.z - c;
+		double zMax = center.z + c;
+		setA((xMax - xMin) / 2.0);
+		setB((yMax - yMin) / 2.0);
+		setC((zMax - zMin) / 2.0);
 		
 		//calc quadratic equation coefficents
-		double A = d.x * d.x / a * a + d.y * d.y / b * b + d.z * d.z / c * c;
-		double B = 2 * (o.x * d.x / a * a + o.y * d.y / b * b + o.z * d.z / c * c);
-		double C = o.x * o.x / a * a + o.y * o.y / b * b + o.z * o.z / c * c - 1;
-
+		// double A = d.x * d.x / a * a + d.y * d.y / b * b + d.z * d.z / c * c;
+		// double B = 2 * (o.x * d.x / a * a + o.y * d.y / b * b + o.z * d.z / c * c);
+		// double C = o.x * o.x / a * a + o.y * o.y / b * b + o.z * o.z / c * c - 1;
+		double A = d.x * d.x / (a * a) + d.y * d.y / (b * b) + d.z * d.z / (c * c);
+		double B = 2 * (d.x * (o.x - center.x) / (a * a) + d.y * (o.y - center.y) / (b * b) + d.z * (o.z - center.z) / (c * c));
+		double C = (o.x - center.x) * (o.x - center.x) / (a * a) + (o.y - center.y) * (o.y - center.y) / (b * b) + (o.z - center.z) * (o.z - center.z) / (c * c) - 1;
+		
 		//solve quadratic for t
 		double[] roots = solveQuadratic(A, B, C);
 
@@ -71,11 +81,19 @@ public class Ellipsoid extends Surface {
 				Point3 intersectionPoint = ray.evaluate(root);
 				
 				//calc normal
+				// Vector3 normal = new Vector3(
+                //     2 * intersectionPoint.x / (a * a),
+                //     2 * intersectionPoint.y / (b * b),
+                //     2 * intersectionPoint.z / (c * c)
+            	// );
+
 				Vector3 normal = new Vector3(
-                    2 * intersectionPoint.x / (a * a),
-                    2 * intersectionPoint.y / (b * b),
-                    2 * intersectionPoint.z / (c * c)
+                2 * (intersectionPoint.x - center.x) / (a * a),
+                2 * (intersectionPoint.y - center.y) / (b * b),
+                2 * (intersectionPoint.z - center.z) / (c * c)
             	);
+
+				normal.normalize();
 
 				// return the hit
 				hit.set(this, root, normal);
