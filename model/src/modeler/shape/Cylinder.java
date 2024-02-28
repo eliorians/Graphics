2bb0c0001861d504ef78e30010a8239528a3369e
Eli Orians <eli.orians@gmail.com>
2024-02-27 19:40:14 -0500

    some cylinder stuff

--- model/src/modeler/shape/Cylinder.java
+++ model/src/modeler/shape/Cylinder.java
@@ -45,53 +45,74 @@ public class Cylinder extends Shape {
 		Vector3f[] normals = generateNormals(numFacets);
 		Point3i[] triangles = generateTriangles(numFacets);
 
-		System.out.println("-------------------------");
-		System.out.println("flatnessTolerance: " + flatnessTolerance);
-		System.out.println("numFacets: " + numFacets);
-		System.out.println("Vertices:");
-		for (Point3f vertex : vertices) {
-			System.out.println(vertex);
-		}
+		// System.out.println("-------------------------");
+		// System.out.println("flatnessTolerance: " + flatnessTolerance);
+		// System.out.println("numFacets: " + numFacets);
+		// System.out.println("Vertices:");
+		// for (Point3f vertex : vertices) {
+		// 	System.out.println(vertex);
+		// }
 
-		// Printing normals
-		System.out.println("Normals:");
-		for (Vector3f normal : normals) {
-			System.out.println(normal);
-		}
+		// // Printing normals
+		// System.out.println("Normals:");
+		// for (Vector3f normal : normals) {
+		// 	System.out.println(normal);
+		// }
 
-		// Printing triangles
-		System.out.println("Triangles:");
-		for (Point3i triangle : triangles) {
-			System.out.println(triangle);
-		}
-		System.out.println("-------------------------");
+		// // Printing triangles
+		// System.out.println("Triangles:");
+		// for (Point3i triangle : triangles) {
+		// 	System.out.println(triangle);
+		// }
+		// System.out.println("-------------------------");
 
 		//Build the mesh data arrays from the static mesh data
 		int vertLength = vertices.length;
 		float[] vertData = new float[3*vertLength];
 		float[] normData = new float[3*vertLength];
-
 		//Create the triangle list
 		int[] triData = new int[3*triangles.length];
+
+		// System.out.println("Lengths");
+		// System.out.println(vertData.length);
+		// System.out.println(normData.length);
+		// System.out.println(triData.length);
 
 		//Copy the vertex data
 		for(int i = 0; i < vertLength; i++) {
 
+			// System.out.println("Triangle: " + i);
+
 			vertData[3*i] = vertices[i].x;
 			vertData[3*i+1] = vertices[i].y;
 			vertData[3*i+2] = vertices[i].z;
+			// System.out.println("Vertices: ");
+			// System.out.println(vertData[3*i]);
+			// System.out.println(vertData[3*i+1]);
+			// System.out.println(vertData[3*i+2]);
+
 			normData[3*i] = normals[i].x;
 			normData[3*i+1] = normals[i].y;
 			normData[3*i+2] = normals[i].z;
-
+			// System.out.println("Normals:");
+			// System.out.println(normData[3*i]);
+			// System.out.println(normData[3*i+1]);
+			// System.out.println(normData[3*i+2]);
+			// System.out.println("");
 		}
 
 		//Copy the triangle data
 		for(int i = 0; i < triangles.length; i++) {
-
+			
 			triData[3*i] = triangles[i].x;
 			triData[3*i+1] = triangles[i].y;
 			triData[3*i+2] = triangles[i].z;
+			
+			System.out.println("Triangle: " + i);
+			System.out.println(triData[3*i]);
+			System.out.println(triData[3*i+1]);
+			System.out.println(triData[3*i+2]);
+			System.out.println("");
 
 		}
 
@@ -115,7 +136,7 @@ public class Cylinder extends Shape {
 
 	Point3f[] generateVertices(int numFacets)
 	{	
-		int numVertices = numFacets*3;
+		int numVertices = numFacets;
 		double angleIncrement = 2 * Math.PI / numFacets;
 		Point3f[] vertices = new Point3f[numVertices];
 
@@ -132,11 +153,11 @@ public class Cylinder extends Shape {
 
 	Vector3f[] generateNormals(int numFacets)
 	{
-		int numNormals = numFacets*3;
+		int numNormals = numFacets;
 		Vector3f[] normals = new Vector3f[numNormals];
 
-		//generate normals for base layer, all upward in the z direction
-		Vector3f baseNormal = new Vector3f(0.0f, 0.0f, 1.0f);
+		//generate normals for base layer, all upward in the y direction
+		Vector3f baseNormal = new Vector3f(0.0f, -1.0f, 0.0f);
 		for (int i = 0; i < numNormals; i++) {
             normals[i] = baseNormal;
         }
@@ -150,7 +171,7 @@ public class Cylinder extends Shape {
 		Point3i[] triangles = new Point3i[numTriangles];
 
 		//generate triangles for base layer
-		for (int i = 0; i < numTriangles; i++) {
+		for (int i = 0; i < numFacets; i++) {
             int vertex1 = 3 * i; 
             int vertex2 = 3 * i + 1;
             int vertex3 = 3 * i + 2;
